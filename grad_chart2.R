538264306b94264c6c7b5e3c1fdcf717e15f84b9
Brian Gulbis <brian.gulbis@gmail.com>
2015-10-20 12:02:37 -0500

    updated points

--- grad_chart2.R
+++ grad_chart2.R
@@ -4,9 +4,6 @@ library(stringr)
 library(ggplot2)
 library(ggmap)
 
-map <- get_map(location = "united states", zoom=4, source="google")
-# ggmap(map)
-
 grads <- read.csv("pgy1_grads.csv", colClasses = "character")
 
 # grads.city <- grads %>%
@@ -26,18 +23,23 @@ grads.jobs <- grads %>%
 # lonlatdf <- do.call(rbind, lonlat)
 # grads.jobs <- cbind(grads.jobs, lonlatdf)
 # 
+
+map <- get_map(location = "united states", zoom=4, source="google")
+
 map1 <- ggmap(map) + 
-    geom_point(aes(x=lon, y=lat, shape=PGY2, color=PGY2), data=grads.jobs, alpha=0.8) 
-    # geom_text(aes(x=lon, y=lat, label=First.Region), data=grads.city)
+    geom_jitter(aes(x=lon, y=lat, color=PGY2), data=grads.jobs, size=4, alpha=0.6) + 
+    geom_jitter(aes(x=lon, y=lat), data=grads.jobs, size=4, color="black", shape=1) +
+    scale_color_manual(values=c("red","blue")) +
+    theme_nothing()
 map1
 
 map.hou <- get_map(location = "houston", source="google")
+map.hou.osm <- get_map(location="houston", source="osm")
+
 map2 <- ggmap(map.hou) + 
-    geom_jitter(aes(x=lon, y=lat, shape=PGY2, color=PGY2), data=grads.jobs, alpha=0.8) +
-    scale_shape_manual(values=c(3, 16)) +
-    scale_color_manual(values=c('#999999','#E69F00')) 
-    # scale_size_manual(values=c(2,3,4))
-    
-# geom_text(aes(x=lon, y=lat, label=First.Region), data=grads.city)
+    geom_jitter(aes(x=lon, y=lat, color=PGY2), data=grads.jobs, size=4, alpha=0.5) +
+    geom_jitter(aes(x=lon, y=lat), data=grads.jobs, size=4, color="black", shape=1) +
+    scale_color_manual(values=c("red","blue")) +
+    theme_nothing()
 map2
 
